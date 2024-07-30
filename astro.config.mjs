8d632e406fa326e01748042183b8ec3e1cedf839
enzodp123 <108824262+enzodp123@users.noreply.github.com>
2024-07-30 20:09:46 -0300

    subida/app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://enzodp123.github.io',
+  base: 'Landing-V1',
   integrations: [tailwind()]
 });
